931910a09b3db19c68e494c37ef79ba0aedd8149
kevinjk27 <52363107+kevinjk27@users.noreply.github.com>
2020-05-24 21:33:44 -0700

    Intial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ITC155-Midterm Spring 2020
---
+++ src/StudentGrades.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class StudentGrades {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Enter a student name: ");
+		String studentName = scnr.nextLine();
+		System.out.println("Enter new grade: ");
+		double studentGrade = scnr.nextDouble();
+
+		HashMap<String, Double> studentGrades = new HashMap<String, Double>();
+		// Students' grades (pre-entered)
+		studentGrades.put("Harry Rawlins", 84.3);
+		studentGrades.put("Stephanie Kong", 91.0);
+		studentGrades.put("Shailen Tennyson", 78.6);
+		studentGrades.put("Quincy Wraight", 65.4);
+		studentGrades.put("Janine Antinori", 98.2);
+
+		if (studentGrades.containsKey(studentName)) {
+			HashMap<String, Double> newStudentGrades = new HashMap<String, Double>();
+			newStudentGrades.put(studentName, studentGrade);
+			System.out.println(studentName + "'s original grade: " + studentGrades.get(studentName));
+			System.out.println(studentName + "'s new grade: " + newStudentGrades.get(studentName));
+			scnr.close();
+		} 
+
+	}
+}
